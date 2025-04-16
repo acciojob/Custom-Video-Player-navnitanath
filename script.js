cff1a9a4917530726b9a71474e593ecb420887c7
navnitanath <nancynath751@gmail.com>
2025-04-16 16:37:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,51 @@
-/* Edit this file */
-const player = document.querySelector('.player');
-const video = player.querySelector('.viewer');
-const progress = player.querySelector('.progress');
-const progressBar = player.querySelector('.progress__filled');
-const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelectorAll('.player__slider');
+const video = document.querySelector('.player__video');
+const toggle = document.querySelector('.toggle');
+const skipButtons = document.querySelectorAll('.skip');
+const sliders = document.querySelectorAll('.player__slider');
+const progress = document.querySelector('.progress');
+const progressFilled = document.querySelector('.progress__filled');
+
+function togglePlay() {
+	if (video.paused) {
+		video.play();
+	} else {
+		video.pause();
+	}
+	
+}
+
+function updateButton() {
+	toggla.textContent = video.paused ? '►' : '❚ ❚';
+}
+
+function skip() {
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+// Handle sliders (volume/playback speed)
+function handleSliderUpdate() {
+    video[this.name] = this.value;
+}
+
+// Update progress bar
+function updateProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressFilled.style.width = `${percent}%`;
+}
+
+// Seek to clicked position
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+
+// Event Listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', updateProgress);
+
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+sliders.forEach(slider => slider.addEventListener('input', handleSliderUpdate));
+progress.addEventListener('click', scrub);
